65a48c1e4793904137b61c9e45a278eb05bcb6df
Lynn Luong <lynn.h.luong@gmail.com>
2022-07-22 15:56:19 -0400

    changed to csrf token

--- react-app/src/store/csrf.js
+++ react-app/src/store/csrf.js
@@ -12,7 +12,7 @@ export async function csrfFetch(url, options = {}) {
   if (options.method.toUpperCase() !== 'GET') {
     options.headers['Content-Type'] =
       options.headers['Content-Type'] || 'application/json';
-    options.headers['XSRF-Token'] = Cookies.get('XSRF-TOKEN');
+    options.headers['csrf_token'] = Cookies.get('csrf_token');
   }
   // call the default window's fetch with the url and the options passed in
   const res = await window.fetch(url, options);
