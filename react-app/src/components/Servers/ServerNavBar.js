e1bbe95558a9f9c560330975235a4d42e770dbd4
Lynn Luong <lynn.h.luong@gmail.com>
2022-07-22 16:57:01 -0400

    started serverNavBar component

---
+++ react-app/src/components/Servers/ServerNavBar.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadServers } from '../../store/servers';
+
+const ServerNavBar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
