0c223717e92f4c92f801c3b06cab1d0d2eaf1f0d
Lynn Luong <lynn.h.luong@gmail.com>
2022-07-22 16:37:54 -0400

    finished store for servers

--- react-app/src/store/servers.js
+++ react-app/src/store/servers.js
@@ -48,6 +48,30 @@ export const createServer = payload => async dispatch => {
   }
 }
 
+export const editServer = payload => async dispatch => {
+  const res = await fetch(`/api/servers/${payload.id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(payload)
+  });
+
+  if (res.ok) {
+    const server = await res.json();
+    dispatch(edit(server));
+    return server;
+  }
+}
+
+export const deleteServer = serverId => async dispatch => {
+  const res = await fetch(`/api/servers/${serverId}`, {
+    method: 'DELETE'
+  })
+
+  if (res.ok) {
+    dispatch(remove(serverId))
+  }
+}
+
 let newState;
 
 export default function serversReducer(state = {}, action) {
@@ -58,10 +82,22 @@ export default function serversReducer(state = {}, action) {
         newState[server.id] = server
       })
       return newState;
+
     case ADD:
       newState = {...state};
       newState[action.server.id] = action.server;
       return newState;
+
+    case EDIT:
+      newState = {...state};
+      newState[action.server.id] = action.server;
+      return newState;
+
+    case REMOVE:
+      newState = {...state}
+      delete newState[action.serverId];
+      return newState;
+
     default:
       return state;
   }
