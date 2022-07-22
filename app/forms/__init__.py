8312e8ff6f64b0522f7325368a522feb5cfb6a38
Lynn Luong <lynn.h.luong@gmail.com>
2022-07-22 18:36:33 -0400

    created channels forms and backend routes

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,3 +2,5 @@ from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .server_form import ServerForm
 from .edit_server_form import EditServerForm
+from .channel_form import ChannelForm
+from .edit_channel_form import EditChannelForm
