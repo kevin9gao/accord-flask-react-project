fc61d251770d872a9f6660f1744c1d09e6321142
erikphinguyen <erikphinguyen@gmail.com>
2022-07-24 19:45:50 -0700

    first two background images done

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -8,17 +8,25 @@ function SplashPage() {
                     <h1>
                         IMAGINE A PLACE...
                     </h1>
-                    <div>
-                        <p className='image-p'>
-                            ...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.
-                        </p>
-                    </div>
+                    <h2>
+                        ...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.
+                    </h2>
                 </div>
                 <div className='splash-image2'>
-
+                    <h1>
+                        Create an invite-only place where you belong
+                    </h1>
+                    <h2>
+                    Accord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat.
+                    </h2>
                 </div>
                 <div className='splash-image3'>
-
+                    <h1>
+                    Where hanging out is easy
+                    </h1>
+                    <h2>
+                    Grab a seat in a voice channel when you’re free. Friends in your server can see you’re around and instantly pop in to talk without having to call.
+                    </h2>
                 </div>
                 <div className='splash-image4'>
 
