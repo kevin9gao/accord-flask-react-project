70763e030cf77b00225c8914deabfa74723221a3
Wanting Zhang <christinezhang@Wantings-MBP.lan>
2022-07-25 13:14:14 -0400

    joining servers and users

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -22,9 +22,9 @@ def user(id):
 @login_required
 def userServers(id):
     print('hitting /api/users/:userid/servers')
-    # user = db.session.query(User.id)
+   
     # servers = user.servers_joined
-    servers = User.query.filter(User.servers_joined).all()
+    servers = session.query(Server).all()
     # print('user:', user)
     print('SERVERS JOINED BY USER', servers)
     return {'servers': [server.to_dict() for server in servers]}
