f5a849dd1ac4ee046d3ee9f5cd3a288d413f9ffe
kevin <kevingao52997@gmail.com>
2022-07-25 22:05:54 -0700

    did some css for discover page

--- app/models/server.py
+++ app/models/server.py
@@ -14,7 +14,8 @@ class Server(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40), nullable=False, unique=True)
     owner_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-
+    server_pic_url = db.Column(db.String(255), nullable=True, default='https://www.crn.com/resources/025f-0fe9595205a2-2a2f315866b2-1000/data-center-servers-supercomputers.jpg')
+    server_cover_pic_url = db.Column(db.String(255), nullable=True, default='https://media.istockphoto.com/photos/abstract-blue-flight-in-space-hyper-jump-3d-rendering-picture-id1288036111?k=20&m=1288036111&s=612x612&w=0&h=Nmjm1IeQQb2oAQhnfvlhbTelZar5s1x7dTpD-1M4rik=')
 
     def to_dict(self):
         return {
--- react-app/src/components/Servers/ServerDiscover.js
+++ react-app/src/components/Servers/ServerDiscover.js
@@ -4,6 +4,7 @@ import * as sessionActions from "../../store/session";
 import * as serverActions from "../../store/servers";
 import { NavLink } from "react-router-dom";
 import JoinServerModal from './JoinServerModal';
+import './ServerDiscover.css';
 
 export default function ServerDiscover() {
     const dispatch = useDispatch();
@@ -11,7 +12,7 @@ export default function ServerDiscover() {
     const servers = useSelector(state => state.servers)
     // console.log("server", servers)
     const serversArray = servers ? Object.values(servers) : null
-    // console.log("server array",serversArray)
+    console.log("server array",serversArray)
 
     useEffect(() => {
         dispatch(serverActions.loadServers());
@@ -19,17 +20,30 @@ export default function ServerDiscover() {
 
 
     return (
-        <div>
+        <div className="main-containers">
             <h2>Click on a Server to Join!</h2>
-            {serversArray && serversArray.map(server => {
-                return (server && (
-                    <div key={server.id}>
-                        <JoinServerModal server={server} />
-                    </div>
-                )
-                )
-            }
-            )}
+            <div className="card-containers">
+                {serversArray && serversArray.map(server => {
+                    return (server && (
+                        <div
+                            key={server.id}
+                        >
+                            <article
+                                className='cards'
+                            >
+                                <img
+                                    src={server.server_cover_pic_url}
+                                    className='cover-pics'
+                                    alt='cover-pics'
+                                />
+                                <JoinServerModal server={server} />
+                            </article>
+                        </div>
+                    )
+                    )
+                }
+                )}
+            </div>
         </div>
     )
 }
