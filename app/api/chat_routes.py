52e5d6886c4e5531c0d855ab1afd638f56add7ae
kevin <kevingao52997@gmail.com>
2022-07-27 16:22:09 -0700

    backend route for GET /chat/live_chat/:channel_id

--- app/api/chat_routes.py
+++ app/api/chat_routes.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, request
-
+from app.models import LiveChatMessage, DirectMessage, db
 
 chat_routes = Blueprint('chat', __name__)
 
-# @chat_routes.route('/live_chat/<int:id>', methods=['GET'])
-# def get_live_chat_messages(id):
+@chat_routes.route('/live_chat/<int:channel_id>', methods=['GET'])
+def get_live_chat_messages(channel_id):
+  chat_messages = LiveChatMessage.query.filter(LiveChatMessage.channel_id == channel_id).all()
+  return {'chat_history': [message.to_dict() for message in chat_messages]}
 
 
 # @chat_routes.route('/live_chat/<int:id>', methods=['POST'])
