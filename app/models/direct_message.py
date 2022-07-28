0d5d5162bc57b89478ea107f1385dfa1d13f02e5
Lynn Luong <lynn.h.luong@gmail.com>
2022-07-28 12:20:04 -0400

    removed unique

--- app/models/direct_message.py
+++ app/models/direct_message.py
@@ -6,8 +6,8 @@ class DirectMessage(db.Model):
     __tablename__ = "direct_messages"
 
     id = db.Column(db.Integer, primary_key=True)
-    sender_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False, unique=True)
-    recipient_id = db.Column(db.Integer, nullable=False, unique=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    recipient_id = db.Column(db.Integer, nullable=False)
     message_body = db.Column(db.String(1000), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, server_default=func.now())
 
