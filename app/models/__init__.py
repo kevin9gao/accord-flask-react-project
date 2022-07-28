9490481f2f2cd36b0c153cc320fecafeaafe3d12
Lynn Luong <lynn.h.luong@gmail.com>
2022-07-28 12:32:52 -0400

    deleted migrations and migrated new table

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,5 @@ from .db import db
 from .user import User
 from .server import Server
 from .channel import Channel
-from .direct_message import DirectMessage, DMConversation
+from .direct_message import DirectMessage
 from .live_chat import LiveChatMessage
