8a7d74e453c93cb2211cb2cd87731afe3fc62508
Wanting Zhang <christinezhang@Wantings-MBP.lan>
2022-07-29 11:34:49 -0400

    links on footer works

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -8,12 +8,12 @@ function Footer() {
                 <div>
                     <li>Connect with Christine:</li>
                     <div>
-                        <a href='https://github.com/erikphinguyen/'>
+                        <a href='https://github.com/tinezh94' target='_blank'>
                             <i className="fab fa-github-alt"></i>
                         </a>
                     </div>
                     <div>
-                        <a href='https://www.linkedin.com/in/erikphinguyen/'>
+                        <a href='https://www.linkedin.com/in/christine-zhang-4b263080/' target='_blank'>
                             <i className="fab fa-linkedin"></i>
                         </a>
                     </div>
@@ -21,12 +21,12 @@ function Footer() {
                 <div>
                     <li>Connect with Erik:</li>
                     <div>
-                        <a href='https://github.com/erikphinguyen/'>
+                        <a href='https://github.com/erikphinguyen/' target='_blank'>
                             <i className="fab fa-github-alt"></i>
                         </a>
                     </div>
                     <div>
-                        <a href='https://www.linkedin.com/in/erikphinguyen/'>
+                        <a href='https://www.linkedin.com/in/erikphinguyen/' target='_blank'>
                             <i className="fab fa-linkedin"></i>
                         </a>
                     </div>
@@ -34,12 +34,12 @@ function Footer() {
                 <div>
                     <li>Connect with Kevin:</li>
                     <div>
-                        <a href='https://github.com/erikphinguyen/'>
+                        <a href='https://github.com/kevin9gao' target='_blank'>
                             <i className="fab fa-github-alt"></i>
                         </a>
                     </div>
                     <div>
-                        <a href='https://www.linkedin.com/in/erikphinguyen/'>
+                        <a href='https://www.linkedin.com/in/kevin-gao-81a7b8241/' target='_blank'>
                             <i className="fab fa-linkedin"></i>
                         </a>
                     </div>
@@ -47,12 +47,12 @@ function Footer() {
                 <div>
                     <li>Connect with Lynn:</li>
                     <div>
-                        <a href='https://github.com/erikphinguyen/'>
+                        <a href='https://github.com/lynnluong04' target='_blank'>
                             <i className="fab fa-github-alt"></i>
                         </a>
                     </div>
                     <div>
-                        <a href='https://www.linkedin.com/in/erikphinguyen/'>
+                        <a href='https://www.linkedin.com/in/lynn-luong-905740139/' target='_blank'>
                             <i className="fab fa-linkedin"></i>
                         </a>
                     </div>
