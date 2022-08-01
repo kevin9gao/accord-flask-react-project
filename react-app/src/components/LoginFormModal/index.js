c705e0b775e2f165f4a857e07180de271daad67f
Lynn Luong <lynn.h.luong@gmail.com>
2022-07-31 21:43:13 -0400

    more css

--- react-app/src/components/LoginFormModal/index.js
+++ react-app/src/components/LoginFormModal/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
 import LoginForm from '../auth/LoginForm';
 import { NavLink } from 'react-router-dom';
-
+import "./LoginModal.css"
 function LoginFormModal() {
     const [showModal, setShowModal] = useState(false);
 
