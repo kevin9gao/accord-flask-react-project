75a44760da111e887129b16abeb9950d719fa779
Lynn Luong <lynn.h.luong@gmail.com>
2022-07-31 20:29:28 -0400

    abort

---
+++ react-app/src/components/ServersDropDown/index.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { deleteServer, leaveServer } from "../../store/servers";
+import EditServerModal from "../Servers/EditServerModal";
+import '../Channels/ChannelsNavBar.css';
+
+const ServerNameDropDown = ({ server }) => {
+
+    const user = useSelector(state => state.session.user)
+    // const userServers = useSelector(state => state?.servers['user-servers'])
+    const [showMenu, setShowMenu] = useState(false);
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    useEffect(() => {
+        if (!showMenu) return;
+    }, [showMenu]);
+
+    const deleteServ = async (id) => {
+        await dispatch(deleteServer(id));
+        history.push('/discover');
+    }
+
+    const leaveServ = async (id) => {
+        // e.preventDefault();
+
+        const payload = {
+            user_id: user?.id,
+            server_id: id
+        }
+        // console.log('FRONTEND, payload', payload)
+        await dispatch(leaveServer(payload))
+    }
+
+    // console.log(server)
+
+    useEffect(() => {
+        setShowMenu(false);
+    }, [server?.id])
+
+    return (
+        <div className="channel-server-div">
+            <h3 className="channel-server-name">{server && (
+                server?.name
+            )}</h3>
+            <button className="dropdown-button" onClick={() => setShowMenu(!showMenu)}>
+                <i className="fa-solid fa-angle-down"></i>
+            </button>
+            {showMenu && (
+                <div className="dropdown-container">
+                    {user?.id === server?.owner_id && (
+                        <div>
+                            <EditServerModal server={server} />
+                            <div className="dropdown-delete-div">
+                                <button className="drp-server-btn" id="dropdown-delete" type="submit" onClick={() => deleteServ(server.id)}>Delete Server</button>
+                                <i className="fa-solid fa-trash-can"></i>
+                            </div>
+                        </div>
+                    )}
+                    <div>
+                        <button className="drp-server-btn" id="dropdown-leave" onClick={() => leaveServ(server.id)}>Leave Server</button>
+                        <i className="fa-solid fa-arrow-right-from-bracket"></i>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default ServerNameDropDown;
---
+++ react-app/src/components/auth/LoginFormDiscover.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { login } from '../../store/session';
+
+const LoginFormDiscover = () => {
+  const [errors, setErrors] = useState([]);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const onLogin = async (e) => {
+    e.preventDefault();
+    const data = await dispatch(login(email, password));
+    if (data) {
+      setErrors(data);
+    }
+    history.push('/discover');
+  };
+
+  const updateEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const updatePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+
+  return (
+    <form className='form' onSubmit={onLogin}>
+      <div>
+        {errors.map((error, ind) => (
+          <div key={ind}>{error}</div>
+        ))}
+      </div>
+      <div className='form-misc'>
+        <label className='login-modal-label' htmlFor='email'>Email</label>
+        <input
+          className='login-modal-input'
+          name='email'
+          type='text'
+          placeholder='Email'
+          value={email}
+          onChange={updateEmail}
+        />
+      </div>
+      <div className='form-misc'>
+        <label className='login-modal-label' htmlFor='password'>Password</label>
+        <input
+           className='login-modal-input'
+          name='password'
+          type='password'
+          placeholder='Password'
+          value={password}
+          onChange={updatePassword}
+        />
+      </div>
+      <div className='form-misc'>
+        <button className='login-splash-submit-btn' type='submit'>Login</button>
+      </div>
+    </form>
+  );
+};
+
+export default LoginFormDiscover;
